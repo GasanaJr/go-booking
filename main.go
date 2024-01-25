2ecaeebad31ad3485d772365bf95ec1f0a535621
GasanaJr <gasanadj2003@gmail.com>
2024-01-25 13:12:34 +0200

    ft: add booking logic

--- main.go
+++ main.go
@@ -11,4 +11,18 @@ func main() {
 	fmt.Println("We have a total of ",
 		confrenceTickets, " tickets and ", remainingTickets,
 		" tickets remaining")
+
+	var userName string
+	var userTicket int
+	fmt.Println("Enter user name")
+	fmt.Scan(&userName)
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTicket)
+
+	// Update tickets
+	remainingTickets = remainingTickets - userTicket
+
+	fmt.Printf("Hello %v, you have %v tickets \n", userName, userTicket)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confrenceName)
+
 }
