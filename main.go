88803c2e4808214d70cca05fa647cc429f9cc164
GasanaJr <gasanadj2003@gmail.com>
2024-01-25 13:56:44 +0200

    ft: added loops

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var confrenceName = "Go Conference"
 	const confrenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v application\n", confrenceName)
 	fmt.Println("We have a total of ",
@@ -16,17 +19,29 @@ func main() {
 	bookings := []string{}
 	//var bookings []string
 
-	var userName string
-	var userTicket int
-	fmt.Println("Enter user name")
-	fmt.Scan(&userName)
-	fmt.Println("Enter number of tickets")
-	fmt.Scan(&userTicket)
+	for {
+		var userName string
+		var lastName string
+		var userTicket uint
+		fmt.Println("Enter user name")
+		fmt.Scan(&userName)
+		fmt.Println("Enter last name")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTicket)
 
-	// Update tickets
-	remainingTickets = remainingTickets - userTicket
-	bookings = append(bookings, userName)
-	fmt.Printf("Hello %v, you have %v tickets \n", userName, userTicket)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confrenceName)
-	fmt.Printf("All the bookings are %v \n", bookings)
+		// Update tickets
+		remainingTickets = remainingTickets - userTicket
+		bookings = append(bookings, userName)
+		fmt.Printf("Hello %v, you have %v tickets \n", userName, userTicket)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confrenceName)
+
+		userNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			userNames = append(userNames, names[0])
+		}
+		fmt.Printf("All the usernames who booked are %v \n", userNames)
+	}
+
 }
