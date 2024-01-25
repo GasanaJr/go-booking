649590657eab89c2db62edb944c81688fcc34dbb
GasanaJr <gasanadj2003@gmail.com>
2024-01-25 14:25:55 +0200

    ft: user data validation

--- main.go
+++ main.go
@@ -23,38 +23,55 @@ func main() {
 		var userName string
 		var lastName string
 		var userTicket uint
+		var email string
 		fmt.Println("Enter user name")
 		fmt.Scan(&userName)
 		fmt.Println("Enter last name")
 		fmt.Scan(&lastName)
+		fmt.Println("Enter email")
+		fmt.Scan(&email)
 		fmt.Println("Enter number of tickets")
 		fmt.Scan(&userTicket)
 
-		if userTicket > remainingTickets {
-			fmt.Println("Invalid number of tickets")
+		// User validation
+		isValidName := len(userName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicket := userTicket > 0 && userTicket <= remainingTickets
+
+		if isValidEmail && isValidTicket && isValidName {
+			// Update tickets
+			remainingTickets = remainingTickets - userTicket
+			bookings = append(bookings, userName+" "+lastName)
+			fmt.Printf("Hello %v, you have %v tickets \n", userName, userTicket)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confrenceName)
+
+			//create username slice
+			userNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				userNames = append(userNames, names[0])
+			}
+			fmt.Printf("All the usernames who booked are %v \n", userNames)
+
+			noTicketsRemaining := remainingTickets == 0
+			if noTicketsRemaining {
+				//end the program
+				fmt.Println("The confrence tickets are SOLD OUT")
+				break
+			}
+		} else {
+			if !isValidEmail {
+				fmt.Println("Please enter a valid email address")
+			}
+			if !isValidName {
+				fmt.Println("Please enter a valid name")
+			}
+			if !isValidTicket {
+				fmt.Println("Please enter a valid number of tickets")
+			}
 			continue
 		}
 
-		// Update tickets
-		remainingTickets = remainingTickets - userTicket
-		bookings = append(bookings, userName)
-		fmt.Printf("Hello %v, you have %v tickets \n", userName, userTicket)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confrenceName)
-
-		//create username slice
-		userNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			userNames = append(userNames, names[0])
-		}
-		fmt.Printf("All the usernames who booked are %v \n", userNames)
-
-		noTicketsRemaining := remainingTickets == 0
-		if noTicketsRemaining {
-			//end the program
-			fmt.Println("The confrence tickets are SOLD OUT")
-			break
-		}
 	}
 
 }
